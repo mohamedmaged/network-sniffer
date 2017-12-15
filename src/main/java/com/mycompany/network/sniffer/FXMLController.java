b20ae93d9fff81f4b0a51f94d42b570283e5370a
mohamed maged <mohamedmaged35@yahoo.com>
2017-12-15 02:27:17 +0200

    initial GUI design

--- src/main/java/com/mycompany/network/sniffer/FXMLController.java
+++ src/main/java/com/mycompany/network/sniffer/FXMLController.java
@@ -5,14 +5,38 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.layout.HBox;
+import static sun.audio.AudioDevice.device;
 
 public class FXMLController implements Initializable {
     
-  
-    
-    @Override
+         
+        @FXML public MenuButton device;
+        @FXML public MenuItem wifi;
+        @FXML public MenuItem eth;
+ @FXML
+    private void wificlicked(ActionEvent event)
+    {
+        device.setText(wifi.getText());
+    }
+ @FXML
+      private void ethclicked(ActionEvent event)
+    {
+        device.setText(eth.getText());
+    }
+
+  @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        wifi.setText("Wifi");
+        eth.setText("ethernet");
+   
+      //  f.setText("wifi");
     }    
 }
